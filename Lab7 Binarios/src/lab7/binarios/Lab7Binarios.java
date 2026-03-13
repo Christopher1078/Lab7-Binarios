aaf24cfabd9c0606af49df7e2aa8aa20f69c6884
Christopher1078 <christortizfun@gmail.com>
2026-03-13 09:53:09 -0600

    Main

--- Lab7 Binarios/src/lab7/binarios/Lab7Binarios.java
+++ Lab7 Binarios/src/lab7/binarios/Lab7Binarios.java
@@ -4,7 +4,8 @@ package lab7.binarios;
 public class Lab7Binarios {
 
     public static void main(String[] args) {
-        
+        GUI gui=new GUI();
+        gui.setVisible(true);
     }
     
 }
